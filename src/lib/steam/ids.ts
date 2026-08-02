d6f9625c8a38d408370f5aea881cd56d92108997
Niall Baum <niall@layers.studio>
2026-08-02 01:00:15 +0100

    chore: add explicit return type to steam64ToSteam32

--- src/lib/steam/ids.ts
+++ src/lib/steam/ids.ts
@@ -1,4 +1,4 @@
-export function steam64ToSteam32(steamId64: string) {
+export function steam64ToSteam32(steamId64: string): string {
   const steam32 = BigInt(steamId64) - BigInt(76561197960265728)
 
   return steam32.toString();
